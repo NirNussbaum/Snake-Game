6a316503fe4047332442c86d617ad41a13216a32
Nir Nussbaum <110348133+NirNussbaum@users.noreply.github.com>
2022-10-06 18:58:51 +0300

    Add files via upload

--- JS/dom.js
+++ JS/dom.js
@@ -57,3 +57,34 @@ export const newGameSpeed = speedUp => {
     clearInterval(gameOn);
     gameOn = setInterval(game, snake.snakeSpeed - speedUp);
 }
+
+//mobile
+
+const arrowUPbut = document.querySelector('.arrowUP');
+const arrowDOWNbut = document.querySelector('.arrowDOWN');
+const arrowLEFTbut = document.querySelector('.arrowLEFT');
+const arrowRIGHTbut = document.querySelector('.arrowRIGHT');
+
+arrowUPbut.addEventListener('click', () => {
+    if (dirY) return;
+    dirX = 0;
+    dirY = -1;
+});
+
+arrowDOWNbut.addEventListener('click', () => {
+    if (dirY) return;
+    dirX = 0;
+    dirY = 1;
+});
+
+arrowLEFTbut.addEventListener('click', () => {
+    if (dirX) return;
+    dirX = -1;
+    dirY = 0;
+});
+
+arrowRIGHTbut.addEventListener('click', () => {
+    if (dirX) return;
+    dirX = 1;
+    dirY = 0;
+});
